4286151564ec2f45d309a65a70deafef4553d5d2
DELL <DELL@DESKTOP-A69LF9C>
2025-10-03 10:49:10 +0530

    completed

---
+++ Tnsif_Java_Dhivyakumaran/src/tnsif/exceptionhandling/WithoutExceptionHandling.java
@@ -0,0 +1,12 @@
+//Program without exception handling
+package tnsif.exceptionhandling;
+public class WithoutExceptionHandling {
+
+	public static void main(String[] args) {
+		int data = 50 / 0;
+		System.out.println("Result is: " + data);
+		System.out.println("the program continues....");
+		
+	}
+	
+}
